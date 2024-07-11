d5a6c5122e6e5eb93dd802150a4fe756221cf7f6
deepeshrai37 <deepeshrai02@gmail.com>
2024-07-11 21:32:22 +0530

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,18 +1,32 @@
-module.exports = [
-  'strapi::logger',
+module.exports = ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-   settings: {
-    cors: {
+  {
+    name: 'strapi::cors',
+    config: {
       enabled: true,
-      origin: ['*'],
+      origin: ['*'], // You can replace '*' with your specific frontend URL for better security
     },
   },
 ];
